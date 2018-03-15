5044a4bc861141d793894eaac6c798d2c892ea6c
Bogdan Kyrychenko <bogdan50e@gmail.com>
2018-03-16 00:27:30 +0200

    POGLYAD-53 auth refactoring

--- ApiClient.js
+++ ApiClient.js
@@ -4,28 +4,30 @@ var httpProxy = require('http-proxy');
 
 var _apiDomain;
 var _apiPort;
+var _apiSecret;
+var _apiClientTargetId;
 var _accessToken;
 
 var poglyadApiClient = function(options){
-	_apiDomain 		= options.apiDomain || '';
-	_apiPort 		= options.apiPort || '';
-	_accessToken 	= options.accessToken || '';
-	
-	return poglyadApiClient;
+  _apiDomain 		= options.apiDomain || '';
+  _apiPort 		= options.apiPort || '';
+  _apiSecret 	= options.apiSecret || '';
+
+  return poglyadApiClient;
 };
 
 /**
  * Set API accessToken for all requests
- * 
+ *
  * @param accessToken
  */
 poglyadApiClient.setAccessToken = function(accessToken){
-	_accessToken = accessToken;
+  _accessToken = accessToken;
 };
 
 /**
  * Config proxy
- * 
+ *
  * @param {
  * @param 	options.expressInstance
  * @param 	options.apiPath
@@ -33,63 +35,82 @@ poglyadApiClient.setAccessToken = function(accessToken){
  * @param }
  */
 poglyadApiClient.proxy = function(req, res, next){
-	var url = 'http://' + _apiDomain + ':' + _apiPort;
-	
-	var proxy = httpProxy.createProxyServer({
-		target: url,
-		changeOrigin: true
-	});
-	proxy.web(req, res);
+  var url = 'http://' + _apiDomain + ':' + _apiPort;
+
+  var proxyOptions = {
+    target: url,
+    changeOrigin: true
+  };
+
+  // handle _apiSecret
+  if (_apiSecret) {
+    proxyOptions.headers = { ApiSecret: _apiSecret };
+  }
+
+  // handle _accessToken
+  if (_accessToken) {
+    proxyOptions.headers = { Authorization: 'Bearer ' + _accessToken };
+  }
+
+  // handle ApiClientTargetId
+  if (_apiClientTargetId) {
+    proxyOptions.headers = { ApiClientTargetId: _apiClientTargetId };
+  }
+
+  var proxy = httpProxy.createProxyServer(proxyOptions);
+
+  proxy.web(req, res);
 };
 
 
 /**
- * Send request for configured API 
- * 
+ * Send request for configured API
+ *
  * @param method
  * @param url
  * @param params
  * @param callback
  */
 poglyadApiClient.request = function(method, url, params, callback){
-	if(!_apiDomain) throw new Error('Module poglyad-api-client not initialised correctly. "apiDomain" must be passed. See more at https://github.com/bodyaka/poglyad-api-client/blob/master/README.md');
-	
-	params = params || {};
-	callback = callback || function(){};
-	
-	var options = {
-		method: method,
-		url: 'http://' + _apiDomain + '/' + url,
-		json: true
-	};
-	
-	// handle token
-	var accessToken = _accessToken;
-	if(params.access_token){
-		accessToken = params.access_token;
-		delete params.access_token;
-	}
-	if(accessToken) options.headers = {'Authorization': 'Bearer ' + accessToken};
-	
-	// attach parameters to request
-	if(method.toLowerCase() == 'get'){
-		options.qs = params;
-	}else{
-		options.body = params;
-	}
-	
-	// send request
-	request(options, function(err, res, data) {
-		if(data && data.error) {
-			return callback(new Error('' + data.error.code + ':' + data.error.message), res, data);
-		} else if(err) {
-			return callback(err)
-		} else if(res.statusCode >= 300) {
-			return callback(new Error('' +res.statusCode + ':' + res.body), res, data);
-		}
-		
-		callback(err, res, data);
-	})
+  if(!_apiDomain) throw new Error('Module poglyad-api-client not initialised correctly. "apiDomain" must be passed. See more at https://github.com/bodyaka/poglyad-api-client/blob/master/README.md');
+
+  params = params || {};
+  callback = callback || function(){};
+
+  var options = {
+    method: method,
+    url: 'http://' + _apiDomain + '/' + url,
+    headers: {},
+    json: true
+  };
+
+  // handle _apiClientSource
+  if (_apiSecret) {
+    options.headers.ApiSecret = _apiSecret;
+  }
+
+  // handle token
+  if(_accessToken) options.headers.Authorization = 'Bearer ' + _accessToken;
+
+  // attach parameters to request
+  if(method.toLowerCase() == 'get'){
+    options.qs = params;
+  }else{
+    options.body = params;
+  }
+
+  // send request
+  request(options, function(err, res, data) {
+    if(data && data.error) {
+      return callback(new Error('' + data.error.code + ':' + data.error.message), res, data);
+    } else if(err) {
+      return callback(err)
+    } else if(res.statusCode >= 300) {
+      return callback(new Error('' +res.statusCode + ':' + res.body), res, data);
+    }
+
+    callback(err, res, data);
+  })
 };
 
 module.exports = poglyadApiClient;
