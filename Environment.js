d4575305875340f11c874f8e52cf463736104380
Bogdan Kyrychenko <bogdan50e@gmail.com>
2018-05-12 16:26:26 +0300

    POGLYAD-104 Environment.js; version 1.4.4

--- Environment.js
+++ Environment.js
@@ -4,53 +4,48 @@ var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
 
 var dbClient = function() {
-	
-	return {
-        /**
-         * Module connect to Client DB. (Init session-store module, ..., etc.)
-         * @param app - express instance
-         * @param configs - object {
-		 * 		name: '',
-		 * 		user: '',
-		 * 		password: '',
-		 * 		host: '',
-		 * 		protocol: '',
-		 * 	}
-         */
-		connect: function(app, configDB, configSession, callback) {
-			var orm = new Sequelize(configDB.name, configDB.user, configDB.password, {
-				host: configDB.host,
-				dialect: configDB.protocol,
-				define: {
-					charset: 'utf8',
-					collate: 'utf8_general_ci'
-				}
-			});		
-			
-			app.use(cookieParser());
-			var SequelizeStore = require('connect-session-sequelize')(expressSession.Store);
-			
-			// init sequelize sessions
-			var instSequelizeStore = new SequelizeStore({db: orm})
-			instSequelizeStore.sync().then(function(){ // recreate table on each server start
-				app.use(expressSession({
-					store: instSequelizeStore,
-					resave: false,
-					saveUninitialized: true,
-					secret: configSession.secret,
-					key: configSession.key,
-					cookie: configSession.cookie
-				}));
-				
-				callback(orm);
-			}); 
-			
-			return orm;
-		}
-	}
+
+  return {
+    /**
+     * Module connect to Client DB. (Init session-store module, ..., etc.)
+     * @param configDB - configuration of DB
+     * @param configSession - configuration of Session
+     * @param callback
+     */
+    connect: function(configDB, configSession, callback) {
+      var orm = new Sequelize(configDB.name, configDB.user, configDB.password, {
+        host: configDB.host,
+        dialect: configDB.protocol,
+        define: {
+          charset: 'utf8',
+          collate: 'utf8_general_ci'
+        }
+      });
+
+      Application.instance.use(cookieParser());
+      var SequelizeStore = require('connect-session-sequelize')(expressSession.Store);
+
+      // init sequelize sessions
+      var instSequelizeStore = new SequelizeStore({db: orm})
+      instSequelizeStore.sync().then(function(){ // recreate table on each server start
+        Application.instance.use(expressSession({
+          store: instSequelizeStore,
+          resave: false,
+          saveUninitialized: true,
+          secret: configSession.secret,
+          key: configSession.key,
+          cookie: configSession.cookie
+        }));
+
+        callback(orm);
+      });
+
+      return orm;
+    }
+  }
 }();
 
 
-module.exports = {	
-	dbClient: dbClient
+module.exports = {
+  dbClient: dbClient
 };
