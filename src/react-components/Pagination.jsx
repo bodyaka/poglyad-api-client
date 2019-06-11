ac7b329092b352e72ecf2ce16fa9aa0535cf4ff2
Bogdan Kyrychenko <bogdan50e@gmail.com>
2019-06-11 22:04:56 +0300

    POGLYAD-335 poglyad-org restructure; component Pagination

---
+++ src/react-components/Pagination.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Pagination extends Component {
+  getLink(page, title) {
+    let key = String(page + title);
+    let className = 'page';
+    if (this.props.paging.page === page) {
+      className += ' active';
+    }
+
+    return <a
+      key={key}
+      onClick={this.props.handleTogglePage.bind(null, page)}
+      href="javascript:void(0)"
+      className={className}>
+      {title}
+    </a>;
+  }
+
+  render() {
+    let pages = [];
+    let paging = this.props.paging;
+
+    if (paging.page > 1) {
+      pages.push(this.getLink(paging.page - 1, '<'));
+    }
+
+    for (let i = 1; i <= paging.pages; i++) {
+      pages.push(this.getLink(i, i));
+    }
+
+    if (paging.page < paging.pages) {
+      pages.push(this.getLink(paging.page + 1, '>'));
+    }
+
+    return <div className="pagination">
+      {pages}
+    </div>;
+  }
+}
+
+export default Pagination;
